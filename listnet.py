9abccfbb5e317a73492e77baf4ab71077faff7cf
masatoomori <23582786+masatoomori@users.noreply.github.com>
2022-05-03 01:04:03 +0900

    Update listnet.py

--- listnet.py
+++ listnet.py
@@ -1,3 +1,5 @@
+import random
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,9 +8,9 @@ import numpy as np
 from sklearn import metrics
 
 class Net(nn.Module):
-	def __init__(self, D):
+	def __init__(self, x_dim):
 		super(Net, self).__init__()
-		self.l1 = nn.Linear(D, 10)
+		self.l1 = nn.Linear(x_dim, 10)
 		self.l2 = nn.Linear(10, 1)
 
 	def forward(self, x):
@@ -29,12 +31,12 @@ def listnet_loss(y_i, z_i):
 	return loss
 
 
-def make_dataset(n_train, n_valid, D):
-	ws = torch.randn(D, 1)
+def make_dataset(n_train, n_valid, x_dim, query_size):
+	ws = torch.randn(x_dim, 1)
 
 	# 勾配を自動で計算する必要があるため、requires_grad=True にする
-	X_train = torch.randn(n_train, D, requires_grad=True)
-	X_valid = torch.randn(n_valid, D, requires_grad=True)
+	X_train = torch.randn(n_train, x_dim, requires_grad=True)
+	X_valid = torch.randn(n_valid, x_dim, requires_grad=True)
 
 	ys_train_score = torch.mm(X_train, ws)		# torch.mm(A, B) = A * B
 	ys_valid_score = torch.mm(X_valid, ws)
@@ -43,7 +45,26 @@ def make_dataset(n_train, n_valid, D):
 	ys_train_rel = torch.Tensor(np.digitize(ys_train_score.clone().detach().numpy(), bins))
 	ys_valid_rel = torch.Tensor(np.digitize(ys_valid_score.clone().detach().numpy(), bins))
 
-	return X_train, X_valid, ys_train_rel, ys_valid_rel
+	# データセットをクエリサイズごとにまとめ、インデックスをリスト化する
+	train_indice = list(range(n_train))
+	train_queries = dict()
+	for train_idx in train_indice:
+		query_num = train_idx // query_size
+		if query_num not in train_queries:
+			train_queries[query_num] = [train_idx]
+		else:
+			train_queries[query_num].append(train_idx)
+
+	valid_indice = list(range(n_valid))
+	valid_queries = dict()
+	for valid_idx in valid_indice:
+		query_num = valid_idx // query_size
+		if query_num not in valid_queries:
+			valid_queries[query_num] = [valid_idx]
+		else:
+			valid_queries[query_num].append(valid_idx)
+
+	return X_train, X_valid, ys_train_rel, ys_valid_rel, train_queries, valid_queries
 
 
 def swapped_pairs(ys_pred, ys_target):
@@ -60,29 +81,32 @@ def swapped_pairs(ys_pred, ys_target):
 	return swapped
 
 
-if __name__ == '__main__':
+def main():
 	n_train = 500
 	n_valid = 100
-	D = 50
+	x_dim = 50
 	n_epochs = 10
-	batch_size = 16
+	query_size = 16
 
-	X_train, X_valid, ys_train, ys_valid = make_dataset(n_train, n_valid, D)
+	X_train, X_valid, ys_train, ys_valid, train_queries, valid_queries = make_dataset(n_train, n_valid, x_dim, query_size)
 
-	net = Net(D)
+	net = Net(x_dim)
 	optimizer = optim.Adam(net.parameters())
 
 	for epoch in range(n_epochs):
 		idx = torch.randperm(n_train)
 
+		# クエリ内で呼び出されるレコードの順序をランダムにする
 		X_train = X_train[idx]
 		ys_train = ys_train[idx]
 
-		cur_batch = 0
-		for it in range(n_train // batch_size):
-			batch_X = X_train[cur_batch: cur_batch + batch_size]
-			batch_ys = ys_train[cur_batch: cur_batch + batch_size]
-			cur_batch += batch_size
+		# クエリの呼び出す順序をランダムにする
+		query_keys = list(train_queries.keys())
+		random.shuffle(query_keys)
+
+		for query_num in query_keys:
+			batch_X = X_train[train_queries[query_num]]
+			batch_ys = ys_train[train_queries[query_num]]
 
 			optimizer.zero_grad()
 			if len(batch_X) > 0:
@@ -96,3 +120,7 @@ if __name__ == '__main__':
 			valid_swapped_pairs = swapped_pairs(valid_pred, ys_valid)
 			ndcg_score = metrics.ndcg_score(ys_valid.numpy().reshape(1, -1), valid_pred.numpy().reshape(1, -1))
 			print(f"epoch: {epoch + 1} valid swapped pairs: {valid_swapped_pairs}/{n_valid * (n_valid - 1) // 2} ndcg: {ndcg_score:.4f}")
+
+
+if __name__ == '__main__':
+	main()
